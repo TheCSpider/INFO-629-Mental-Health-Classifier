5133ea7a57a684bd09c7ed9415b09cb491dfd18e
Shad Scarboro <ShadRS@gmail.com>
2026-02-13 20:07:02 -0500

    Test push

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.write("Mental Health Classifier")
 st.write("Student Project for INFO-629 class")
+st.write("This is a placeholder for the mental health classifier application.")
